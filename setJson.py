90c2af6eb7e199ea8a842080767712e75ec93e7b
jezlo <70851273+jezlo@users.noreply.github.com>
2020-09-14 10:13:23 -0500

    Creado archivo setJson
    Se creo el archivo de pruebas para manipulas archivos json

---
+++ setJson.py
@@ -0,0 +1,15 @@
+import json
+
+data = {}
+ds = {}
+ds[dsName] = []
+data['dataSources'] = []
+
+data['dataSources'].append({
+	ds[dsName].apped({
+		'max': max
+	})
+})
+
+with open('archivJson.txt','w') as outfile:
+	json.dump(data, outfile)
