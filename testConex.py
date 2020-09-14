b90128a8ee945bbda5ae9d5ad2274dd8e4431b81
jezlo <70851273+jezlo@users.noreply.github.com>
2020-09-14 09:17:59 -0500

    Update testConex.py

--- testConex.py
+++ testConex.py
@@ -2,10 +2,10 @@ import json
 
 
 def getDatosConex(): 
- with open('config.json') as json_files:
-	data = json.load(json_files)
-	for p in data['conexion']:
-		print('Usuario: '+p['usuario'])
-		print('Password: '+p['password'])
-		print('URL: '+p['url'])
+	with open('config.json') as json_files:
+		data = json.load(json_files)
+		for p in data['conexion']:
+			print('Usuario: '+p['usuario'])
+			print('Password: '+p['password'])
+			print('URL: '+p['url'])
 getDatosConex
