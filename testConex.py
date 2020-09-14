e4b3781f9924d324cb25ec6d2965b6c51a5d3170
Jesus Mendez <jezloss@gmail.com>
2020-09-14 14:15:50 +0000

    Se creo el archivo prueba de conexion

---
+++ testConex.py
@@ -0,0 +1,11 @@
+import json
+
+
+def getDatosConex(): 
+ with open('config.json') as json_files:
+	data = json.load(json_files)
+	for p in data['conexion']:
+		print('Usuario: '+p['usuario'])
+		print('Password: '+p['password'])
+		print('URL: '+p['url'])
+getDatosConex
