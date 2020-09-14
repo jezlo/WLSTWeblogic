9b5799fd99f9a04ce841fc51fa3239034e431976
jezlo <70851273+jezlo@users.noreply.github.com>
2020-09-14 09:56:27 -0500

    agregado ciclo para validar DS
    Se agrego un ciclo para recorrer cada DataSource y mostrar parametro básicos

--- getDSInfo.py
+++ getDSInfo.py
@@ -10,4 +10,22 @@ icts = cmo.getInactiveConnectionTimeoutSeconds()
 print "Las conexiones iniciales son: "+str(ic)
 print "Las conexiones Maximas son: "+str(max)
 print "Las conexiones minimas son: "+str(min)
-print "El timeout de conexion inactiva: "+str(icts) 
+print "El timeout de conexion inactiva: "+str(icts)
+
+cd('/')
+dss = cmo.getJDBCSystemResources()
+
+for ds in dss:
+ dsName = ds.getName()
+ cd('JDBCSystemResources/'+dsName+'/JDBCResource/'+dsName+'/JDBCConnectionPoolParams/'+dsName)
+ ic = cmo.getInitialCapacity()
+ max = cmo.getMaxCapacity()
+ min = cmo.getMinCapacity()
+ icts = cmo.getInactiveConnectionTimeoutSeconds()
+
+ print "Los parametros del Datasource '+dsName+' son:'
+ print "Las conexiones iniciales son: "+str(ic)
+ print "Las conexiones Maximas son: "+str(max)
+ print "Las conexiones minimas son: "+str(min)
+ print "El timeout de conexion inactiva: "+str(icts)
+ cd('/')
